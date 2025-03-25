7cb4eab7c7b9e51bec3e85503610b2068ced4d1d
genizy <185154684+genizy@users.noreply.github.com>
2025-03-24 23:23:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const assetURL = "https://cdn.jsdelivr.net/gh/gn-math/assets";
 let zones = [];
 async function listZones() {
     try {
-        const response = await fetch(zonesURL);
+        const response = await fetch(zonesURL+"?="+Date.now());
         const json = await response.json();
         zones = json.sort((a, b) => a.name.localeCompare(b.name));
         displayZones(zones);
